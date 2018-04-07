5a8b2c6c1b523d3be5a9002fe217fb14c231912f
Btrugman3 <btrugman@gatech.edu>
2018-04-07 17:30:51 -0400

    Added updated sproc

--- PingItWebsite/Stored Procedures/GetGoogleTestResults.sql
+++ PingItWebsite/Stored Procedures/GetGoogleTestResults.sql
@@ -1,13 +1,14 @@
-CREATE DEFINER=`root`@`%` PROCEDURE `GetGoogleTestResults`(IN c VARCHAR(100), IN s VARCHAR(10), IN b VARCHAR(25), IN ordering bool)
+CREATE DEFINER=`root`@`%` PROCEDURE `GetGoogleTestResults`(IN ucity VARCHAR(100), IN ustate VARCHAR(10), IN ubrowser VARCHAR(25), IN uwebsite VARCHAR(100), IN ordering bool)
 BEGIN
 	SELECT w.tstamp, score, category, resources, g.hosts, bytes, html_bytes, css_bytes, image_bytes, webspeed, w.city, w.state, w.browser
     FROM googletests g
     JOIN webtests w 
 		ON w.guid = g.guid 
-			AND ((c = 'null' AND b = 'null')
-				OR (c = 'null' AND w.browser = b)
-                OR (w.city = c AND w.state = s AND b = 'null')
-                OR (w.city = c AND w.state = s AND w.browser = b))
+			AND ((ucity = 'null' AND ubrowser = 'null')
+				OR (ucity = 'null' AND w.browser = ubrowser)
+                OR (w.city = ucity AND w.state = ustate AND ubrowser = 'null')
+                OR (w.city = ucity AND w.state = ustate AND w.browser = ubrowser))
+			AND (uwebsite = 'null' OR w.url LIKE concat('%', uwebsite, '%'))
 	ORDER BY
 		CASE WHEN ordering = TRUE THEN tstamp END ASC,
 		CASE WHEN ordering = FALSE THEN tstamp END DESC;
